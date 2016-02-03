a5a5a2a53b7fbe3bd7bf85991f5f236cd4cb1c20
arryanggaputra <arryanggaputra@gmail.com>
2016-02-03 22:30:42 +0700

    Add dashboard, Signin, Api\Links, in route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,17 +12,18 @@
  */
 
 Route::get('/', ['as' => 'homepage', 'uses' => 'HomepageController@index']);
+Route::get('/dashboard', ['as' => 'dashboard', 'middleware' => 'auth', 'uses' => 'HomepageController@dashboard']);
 /**
  * Authentication and Registration Process
  */
-Route::get('/signin', ['as' => 'signin.index', 'uses' => 'HomepageController@getSignin']);
+Route::get('/signin', ['as' => 'signin.index', 'uses' => 'Auth\AuthController@getLogin']);
 Route::post('/signin', ['as' => 'user.signin', 'uses' => 'Auth\AuthController@postLogin']);
 Route::get('/signout', ['as' => 'user.signout', 'uses' => 'Auth\AuthController@getLogout']);
 Route::get('/signup', ['as' => 'signup.index', 'uses' => 'Auth\AuthController@getRegister']);
 Route::post('/signup', ['as' => 'signup.store', 'uses' => 'Auth\AuthController@postRegister']);
 
 Route::group(['prefix' => 'api'], function () {
-    Route::get('/links', ['as' => 'links.index', 'uses' => 'Api\LinksController@index']);
+    Route::get('/links', ['as' => 'links.index', 'middleware' => 'auth', 'uses' => 'Api\LinksController@index']);
     Route::get('/links/{short_url}', ['as' => 'links.show', 'uses' => 'Api\LinksController@show']);
     Route::post('/links', ['as' => 'links.store', 'uses' => 'Api\LinksController@store']);
 });
