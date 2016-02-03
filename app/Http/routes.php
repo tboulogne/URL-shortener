849248b8403ac25f1d69f5758b8d238abee0cc2e
arryanggaputra <arryanggaputra@gmail.com>
2016-02-03 21:44:16 +0700

    Add routing for Authenticate, Register, and Redirection

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,20 @@
 |
  */
 
+Route::get('/', ['as' => 'homepage', 'uses' => 'HomepageController@index']);
+/**
+ * Authentication and Registration Process
+ */
+Route::get('/signin', ['as' => 'signin.index', 'uses' => 'HomepageController@getSignin']);
+Route::post('/signin', ['as' => 'user.signin', 'uses' => 'Auth\AuthController@postLogin']);
+Route::get('/signout', ['as' => 'user.signout', 'uses' => 'Auth\AuthController@getLogout']);
+Route::get('/signup', ['as' => 'signup.index', 'uses' => 'Auth\AuthController@getRegister']);
+Route::post('/signup', ['as' => 'signup.store', 'uses' => 'Auth\AuthController@postRegister']);
+
 Route::group(['prefix' => 'api'], function () {
     Route::get('/links', ['as' => 'links.index', 'uses' => 'Api\LinksController@index']);
     Route::get('/links/{short_url}', ['as' => 'links.show', 'uses' => 'Api\LinksController@show']);
     Route::post('/links', ['as' => 'links.store', 'uses' => 'Api\LinksController@store']);
 });
+
+Route::get('/{any}', ['as' => 'shorten.redirect', 'uses' => 'HomepageController@getRedirect']);
