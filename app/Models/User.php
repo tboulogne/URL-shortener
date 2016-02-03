d7fed6f2cb4bc0727a63d072adf0aa70977148d5
arryanggaputra <arryanggaputra@gmail.com>
2016-02-03 21:47:17 +0700

    Add token column as Fillable when new Users is registered

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,7 @@ CanResetPasswordContract
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'token'];
 
     /**
      * The attributes excluded from the model's JSON form.
